62b8f35e60414136c9e6d22f59a04131acc4618b
Sagar <sagarwaghmare1384@gmail.com>
2025-10-19 02:19:33 +0530

    Deploy error

--- app/api/worker/jobs/route.ts
+++ app/api/worker/jobs/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '@/lib/mongodb';
 import Job from '@/models/Job';
 import User from '@/models/User';
+import mongoose from 'mongoose';
 import { verifyToken } from '@/lib/auth';
 
 export const dynamic = 'force-dynamic';
